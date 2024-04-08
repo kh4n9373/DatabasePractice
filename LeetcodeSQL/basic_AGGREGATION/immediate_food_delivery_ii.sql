74c4350a5ad6cf9512a2617e844617373c0c64db
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-04-08 10:50:43 +0700

    8/4

---
+++ LeetcodeSQL/basic_AGGREGATION/immediate_food_delivery_ii.sql
@@ -0,0 +1,6 @@
+SELECT ROUND(AVG(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+FROM Delivery
+WHERE (customer_id , order_date) in
+(SELECT customer_id, MIN(order_date)
+FROM Delivery
+GROUP BY customer_id)
